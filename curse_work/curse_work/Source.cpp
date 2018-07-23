c744a1799316386252f79c8d6bbd788286336ad4
Unknown <van9dubstep@mail.ru>
2018-07-23 12:20:59 +0500

    курс

--- curse_work/curse_work/Source.cpp
+++ curse_work/curse_work/Source.cpp
@@ -1,12 +1,15 @@
-﻿#include <fstream>
-#include <iostream>
-#include <string>
+﻿#include <fstream> 
+#include <iostream> 
+#include <string> 
+
 using namespace std;
 
 template <class T>
-class List {
+class List
+{
 private:
-	struct Node {
+	struct Node
+	{
 		T data;
 		Node *Prev, *Next;
 	};
@@ -52,7 +55,8 @@ public:
 		quantity++;
 	};
 	void DeleteById(int _id) {
-		if (_id < 0 || _id >= quantity) return;
+		if (_id < 0 || _id >= quantity)
+			return;
 		Node *Temp = Head;
 		Node *Helping = Head;
 
@@ -71,40 +75,70 @@ public:
 	int size() {
 		return quantity;
 	};
+
+
+
 };
+class MyString
+{
+	char *str;
+};
+
 
 struct flightData {
 	string destination;
 	int num;
 	string fio;
 	string date;
+	//char* destination = new char[100]; 
+	//int num; 
+	//char* fio = new char[100]; 
+	//char* date = new char[100]; 
 };
 
-flightData parseFlightData(string _str) {
-	int pos1 = _str.find(",");
-	string first = _str.substr(0, pos1);
-	int pos2 = _str.find(",", pos1 + 2);
-	int second = stoi(_str.substr(pos1 + 2, pos2 - pos1 - 2));
-	int pos3 = _str.find(",", pos2 + 2);
-	string third = _str.substr(pos2 + 2, pos3 - pos2 - 2);
-	int pos4 = _str.find(",", pos3 + 2);
-	string fourth = _str.substr(pos3 + 2, pos4 - pos3 - 2);
+class Despetcher
+{
 
-	return { first, second, third, fourth };
-}
+private:List<flightData> flightlist;
 
-void search(List<flightData> &flightlist, int num, string date) {
-	for (int i = 0; i < flightlist.size(); i++) {
-		if (flightlist[i].num == num && flightlist[i].date == date)
-			cout << flightlist[i].destination << ", "
-			<< flightlist[i].num << ", " << flightlist[i].fio
-			<< ", " << flightlist[i].date << endl;
+		// string _str; int num; 
+		//добавить сюда flightdata 
+		//убрать flightdata из поиска 
+		//убрать string не библиотеку ( оставить getline) 
+
+public:
+	flightData parseFlightData(string _str)
+	{
+		int pos1 = _str.find(",");
+		string first = _str.substr(0, pos1);
+		int pos2 = _str.find(",", pos1 + 2);
+		int second = stoi(_str.substr(pos1 + 2, pos2 - pos1 - 2));
+		int pos3 = _str.find(",", pos2 + 2);
+		string third = _str.substr(pos2 + 2, pos3 - pos2 - 2);
+		int pos4 = _str.find(",", pos3 + 2);
+		string fourth = _str.substr(pos3 + 2, pos4 - pos3 - 2);
+		return { first, second, third, fourth };
 	}
-}
+	void search(int num)
+	{
+		for (int i = 0; i < flightlist.size(); i++)
+		{
+			if (flightlist[i].num == num)
+				cout << flightlist[i].destination << ", "
+				<< flightlist[i].num << ", " << flightlist[i].fio
+				<< ", " << flightlist[i].date << endl;
+		}
+	}
 
-void main() {
+};
+
+
+
+int main()
+{
 	setlocale(LC_ALL, "Russian");
-
+	Despetcher FlightDate;
+	Despetcher SearchDate;
 	List<flightData> flightlist;
 
 	ifstream fin("data.txt");
@@ -113,34 +147,38 @@ void main() {
 			string entry;
 			getline(fin, entry);
 			if (entry == "") break;
-			flightlist.Add(parseFlightData(entry));
+			flightlist.Add(FlightDate.parseFlightData(entry));
 		}
 	}
 	fin.close();
 
-	while (true) {
+	while (true)
+	{
 		cout << "\nУчет заявок на авиабилеты:";
 		cout << "\n0 -> Выход";
 		cout << "\n1 -> Добавить заявку";
 		cout << "\n2 -> Удалить заявку";
 		cout << "\n3 -> Все заявки";
-		cout << "\n4 -> Поиск заявки по номеру рейса и дате вылета\n";
+		cout << "\n4 -> Поиск заявки по номеру рейса \n";
 		int input;
 		cin >> input;
 		if (input == 0) break;
-		switch (input) {
-		case 1: {
+		switch (input)
+		{
+		case 1:
+		{
 			cout << "Введите пункт назначения, номер рейса, ";
 			cout << "фамилию с инициалами и желаемую дату ";
 			cout << "вылета через запятую:\n";
 			cin >> ws;
 			string entry;
 			getline(cin, entry);
-			flightlist.Add(parseFlightData(entry));
+			flightlist.Add(FlightDate.parseFlightData(entry));
 			cout << "Заявка добавлена.\n";
 			break;
 		}
-		case 2: {
+		case 2:
+		{
 			cout << "Введите номер заявки для удаления:\n";
 			int id;
 			cin >> id;
@@ -150,7 +188,8 @@ void main() {
 		}
 		case 3: {
 			cout << "Все заявки (" << flightlist.size() << "):\n";
-			for (int i = 0; i < flightlist.size(); i++) {
+			for (int i = 0; i < flightlist.size(); i++)
+			{
 				cout << i << ". " << flightlist[i].destination
 					<< ", " << flightlist[i].num
 					<< ", " << flightlist[i].fio
@@ -158,21 +197,25 @@ void main() {
 			}
 			break;
 		}
-		case 4: {
-			cout << "Введите номер рейса и дату вылета:\n";
+		case 4:
+		{
+			cout << "Введите номер рейса :\n";
 			int n;
 			string date;
 			cin >> n >> date;
-			search(flightlist, n, date);
+			SearchDate.search(n);
 			break;
 		}
 		}
+
 	}
 
 	ofstream of("data.txt");
-	if (of) {
+	if (of)
+	{
 		string entry;
-		for (int i = 0; i < flightlist.size(); i++) {
+		for (int i = 0; i < flightlist.size(); i++) 
+		{
 			of << flightlist[i].destination
 				<< ", " << flightlist[i].num
 				<< ", " << flightlist[i].fio
